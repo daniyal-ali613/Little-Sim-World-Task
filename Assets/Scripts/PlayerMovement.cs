057506fc285509aea978e0f2ac503c344d133689
Syed Daniyal Ali <54919310+Dany-Gamer@users.noreply.github.com>
2022-07-04 17:21:33 +0500

    Added Player Animations

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour
 {
     public float moveSpeed = 5f;
-
     public Rigidbody2D rb;
+    public Animator animator;
 
     Vector2 movement;
     void Start()
@@ -18,6 +18,13 @@ public class PlayerMovement : MonoBehaviour
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
+
+
+
     }
 
     private void FixedUpdate()
