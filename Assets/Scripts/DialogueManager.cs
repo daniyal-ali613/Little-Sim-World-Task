3eb4d4264807d3f19a2f36b8f448d2fec86f998f
Syed Daniyal Ali <54919310+Dany-Gamer@users.noreply.github.com>
2022-07-05 04:15:12 +0500

    Added Main Menu, Added Music

--- Assets/Scripts/DialogueManager.cs
+++ Assets/Scripts/DialogueManager.cs
@@ -34,13 +34,23 @@ public class DialogueManager : MonoBehaviour
     {
         if(sentences.Count == 0)
         {
-            Debug.Log("Check");
             EndDialogue();
             return;
         }
 
         string sentence = sentences.Dequeue();
-        dialogueText.text = sentence;
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
     }
 
     void EndDialogue()
