94b10ab7302a3b514af0f816c64ea5c3e74e1189
Syed Daniyal Ali <54919310+Dany-Gamer@users.noreply.github.com>
2022-07-05 03:36:35 +0500

    Added Player Animations, Added Dialogue System

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,11 +7,17 @@ public class PlayerMovement : MonoBehaviour
     public float moveSpeed = 5f;
     public Rigidbody2D rb;
     public Animator animator;
-
+    public SpriteRenderer spriteRenderer;
+    [SerializeField] float radius = 5f;
+    [SerializeField] Transform shop;
     Vector2 movement;
-    void Start()
+    private float trans;
+    float distanceToTarget = Mathf.Infinity;
+    public DialogueTrigger trigger;
+
+    private void Start()
     {
-        
+        trans = transform.localScale.x;
     }
 
     void Update()
@@ -19,16 +25,38 @@ public class PlayerMovement : MonoBehaviour
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
 
+
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
 
+        distanceToTarget = Vector3.Distance(shop.position, transform.position);
 
+        if(distanceToTarget <= radius)
+        {
+            trigger.TriggerDialogue();
+        }
 
+        if(movement.x <= 0.0f)
+        {
+            trans = -1;
+        }
+
+        else
+        {
+            trans = 1;
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+        rb.MovePosition(rb.position + movement.normalized * moveSpeed * Time.fixedDeltaTime);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, radius);
+
     }
 }
