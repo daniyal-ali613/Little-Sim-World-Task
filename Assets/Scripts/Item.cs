5e2b27b4be3b9f44f55963037dd508ec20d41d20
Syed Daniyal Ali <54919310+Dany-Gamer@users.noreply.github.com>
2022-07-06 05:15:29 +0500

    Added Shop UI

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public  List<Sprite> items = new List <Sprite>();
+    public enum ItemType
+    {
+        Red,
+        Green,
+        Black,
+        Yellow,
+        Blue
+    }
+
+    public static int GetCost(ItemType itemType)
+    {
+        switch (itemType)
+        {
+            default:
+            case ItemType.Red:     return 30;
+            case ItemType.Green:   return 50;
+            case ItemType.Black:   return 25;
+            case ItemType.Yellow:  return 20;
+        }
+    }
+
+    public static Sprite GetSprite(ItemType itemType)
+    {
+        switch (itemType)
+        {
+           
+            default:
+            case ItemType.Red:    return items[0];
+            case ItemType.Green:  return items[1];
+            case ItemType.Black:  return items[2];
+            case ItemType.Yellow: return items[3];
+        }
+    }
+}
---
+++ Assets/Scripts/Ui_Shop.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Ui_Shop : MonoBehaviour
+{
+    private Transform container;
+    private Transform shopItemTamplate;
+
+
+    private void Awake()
+    {
+        container = transform.Find("container");
+        shopItemTamplate = container.Find("shopItemTemplate");
+        shopItemTamplate.gameObject.SetActive(false);
+    }
+
+    private void CreateItemButton(Sprite itemSprite, string itemName, int itemCost, int positionIndex)
+    {
+        Transform shopItemTansform = Instantiate(shopItemTamplate, container);
+        RectTransform shopItemRectTransform = shopItemTamplate.GetComponent<RectTransform>();
+
+        float shopItemHeight = 30f;
+        shopItemRectTransform.anchoredPosition = new Vector2(0, -shopItemHeight * positionIndex);
+
+        shopItemRectTransform.Find("itemName").GetComponent<TextMeshProUGUI>().SetText(itemName);
+        shopItemTansform.Find("costText").GetComponent<TextMeshProUGUI>().SetText(itemCost.ToString());
+
+        shopItemTansform.Find("itemImage").GetComponent<Image>().sprite = itemSprite;
+    }
+
+}
