aba6970fde7de37a4b2d0b61621c6fb4f2c746ea
Syed Daniyal Ali <54919310+Dany-Gamer@users.noreply.github.com>
2022-07-07 04:25:34 +0500

    Added Boundries

--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -40,7 +40,7 @@ public class PlayerMovement : MonoBehaviour
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("shop"))
         {
