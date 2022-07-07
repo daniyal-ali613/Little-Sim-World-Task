65a1d3873a2eccd494bc944111ae836d654c3b1a
Syed Daniyal Ali <54919310+Dany-Gamer@users.noreply.github.com>
2022-07-07 05:04:09 +0500

    Updated Menu

---
+++ Assets/Scripts/Menu Script/Menu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+    public GameObject menu;
+
+    public void CloseMenu()
+    {
+        menu.gameObject.SetActive(false);
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown("escape"))
+        {
+            menu.gameObject.SetActive(true);
+        }
+    }
+}
