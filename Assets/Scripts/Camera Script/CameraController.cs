bcb46b492d9d5bf3c7af0fcbda56a7fca0938ef9
Syed Daniyal Ali <54919310+Dany-Gamer@users.noreply.github.com>
2022-07-07 21:25:50 +0500

    Improved Camera Control, imrpoved Charachter Control

--- Assets/Scripts/Camera Script/CameraController.cs
+++ Assets/Scripts/Camera Script/CameraController.cs
@@ -4,24 +4,25 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public Transform target;
-    public float smoothing;
-    public Vector2 minPosition;
-    public Vector2 maxPosition;
+    public Transform target; 
+    public float smoothing = 0.125f;
 
-    private void FixedUpdate()
+    public Vector2 maxPos;
+    public Vector2 minPos;
+
+
+    private void LateUpdate()
     {
-        //transform.position = new Vector3(target.transform.position.x, target.transform.position.y, transform.position.z);
-
         if(transform.position != target.position)
         {
-            Vector3 targetPosition = new Vector3(target.position.x, target.position.y, transform.position.z);
+            Vector3 targetPos = new Vector3(target.position.x, target.position.y, transform.position.z);
 
-            targetPosition.x = Mathf.Clamp(targetPosition.x, minPosition.x, maxPosition.x);
-            targetPosition.y = Mathf.Clamp(targetPosition.y, minPosition.y, maxPosition.y);
+            targetPos.x = Mathf.Clamp(targetPos.x, minPos.x, maxPos.x);
+            targetPos.y = Mathf.Clamp(targetPos.y, minPos.y, maxPos.y);
 
-            transform.position = Vector3.Lerp(transform.position, targetPosition, smoothing);
+            transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
         }
+        
     }
 
 }
--- Assets/Scripts/Player Scripts/PlayerMovement.cs
+++ Assets/Scripts/Player Scripts/PlayerMovement.cs
@@ -8,15 +8,12 @@ public class PlayerMovement : MonoBehaviour
     public Rigidbody2D rb;
     public Animator animator;
     public SpriteRenderer spriteRenderer;
-    [SerializeField] Transform shop;
+    public Transform shop;
     Vector2 movement;
-    private float trans;
     public DialogueTrigger trigger;
+    public Transform target;
+    public float radius;
 
-    private void Start()
-    {
-        trans = transform.localScale.x;
-    }
 
     void Update()
     {
@@ -27,17 +24,6 @@ public class PlayerMovement : MonoBehaviour
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
-
-
-        if(movement.x <= 0.0f)
-        {
-            trans = -1;
-        }
-
-        else
-        {
-            trans = 1;
-        }
     }
 
     private void OnCollisionEnter2D(Collision2D other)
